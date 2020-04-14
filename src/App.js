b05a671dc8aa1af9d6b04038353899b45b9fcfbc
victor <victor,monroy@ipswebtech.com>
2020-04-14 17:10:11 -0500

    practica de recat

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Menu from './Menu';
+import List from './List';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  
+  constructor(props){
+    super(props);
+
+    this.state = {
+      books:[
+        {id:0, rating: 1, title: 'Harry potter y el caliz de fuego', image:'libro01.jpg'},
+        {id:1, rating: 2, title: 'The shinig', image:'libro02.jpg'},
+        {id:2, rating: 3, title: 'Codigo da vinci', image: 'libro03.jpg'},
+        {id:3, rating: 4, title: 'El principito', image: 'libro04.jpg'},
+        {id:4, rating: 5, title: 'Sobrenatural', image:'libro05.jpg'}
+      ],
+      copyBooks: []
+    };
+  }
+
+  componentDidMount(){
+    this.initBooks();
+  }
+
+  initBooks = () => {
+    this.setState((state,props)=>({
+      copyBooks: [...state.books]
+    }));
+
+  }
+
+  onAdd = (item) => {
+    let temp = [...this.state.books];
+    const id =temp[temp.length-1].id + 1;
+    item['id'] = id;
+    temp.push(item);
+
+    this.setState({books: [...temp]});
+    this.initBooks();
+  }
+
+  onSearch = (query) =>{
+    if(query === ''){
+      this.initBooks(); 
+    }else{
+      const temp = [...this.state.books];
+      let res = [];
+
+      temp.forEach(item=>{
+        if(item.title.toLowerCase().indexOf(query) > -1){
+          res.push(item);
+        }
+      });
+      this.setState({copyBooks: [...res]});
+    }
+  }
+  onUpdateRating = (item)=>{
+    var temp = [...this.state.books];
+    const index = temp.findIndex(x => x.id === item.id);
+    
+    temp[index].title = item.title;
+    temp[index].image = item.image;
+    temp[index].rating = item.rating;
+
+    this.setState({books: [...temp]});
+    this.initBooks();    
+  }
+  render (){
+    return (
+      <div className="app">
+        <Menu title="Mis libros favoritos" onadd={this.onAdd} onsearch={this.onSearch}/>
+        <List items={this.state.copyBooks} onupdaterating={this.onUpdateRating}/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/PanelAdd.js
@@ -0,0 +1,68 @@
+import React from 'react'
+
+class  PanelAdd extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state ={
+        title: '',
+        image: '',
+        rating: 1
+        
+
+        };
+    }
+    onChangeTitle = (e)=>{
+        this.setState({title: e.target.value});
+    }
+    onChangeImage = (e)=>{
+        this.setState({image: e.target.value});
+    }
+    onChangeRating = (e)=>{
+        const rating = parseInt(e.target.value)
+        this.setState({rating: rating});
+    }
+
+    onsubmit = (e) => {
+        e.preventDefault();
+        const title = this.state.title;
+        const image = this.state.image;
+        const rating = this.state.rating;
+
+        this.props.onadd({title: title, image: image, rating: rating});
+        this.props.oncancel();
+    }
+
+    render(){
+    return(
+        <div className="new-item-panel-container">
+            <div className="new-item-panel">
+                <form onSubmit={this.onsubmit}>
+                    <p>
+                        <label onChange={this.onChangeTitle}>Titulo del libro</label>
+                        <input type="text" name="title" classNameI="input"></input>
+                    </p>
+                    <p>
+                        <label onChange={this.onChangeImage}>Titulo del imagen</label>
+                        <input type="text" name="image" classNameI="input"></input>
+                    </p>
+                    <p>
+                        <label>Clasificacion</label>
+                        <select onChange={this.onChangeRating}>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                        </select>
+                    </p>
+                    <input type="submit" className="button btn-blue" value="Registrar libro"></input>
+                    <button onClick={this.props.oncancel} className="button btn-blue">Cancelar</button>
+                </form>
+            </div>
+        </div>
+    );
+    }
+}
+
+export default PanelAdd;
